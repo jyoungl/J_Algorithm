dab91f5d38bf7160af51b152f7a468936378ac9c
jyoungl <50093993+jyoungl@users.noreply.github.com>
2023-09-26 15:17:41 +0900

    0926 스터디 업로드

---
+++ 9월/0926/BJ_11053_카드구매하기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_11052_카드구매하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] card = new int[N+1];
+        for(int i=1;i<=N;i++)
+            card[i] = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[1001];
+        dp[1] = card[1];
+        dp[2] = Math.max(card[2], dp[1]+dp[1]);
+
+        for(int i=3;i<=N;i++){
+            dp[i] = card[i];
+            for(int j=i-1;j>i/2;j--){
+                dp[i] = Math.max(dp[i], dp[j]+dp[i-j]);
+            }
+            if(i%2==0)
+                dp[i] = Math.max(dp[i], dp[i/2]+dp[i/2]);
+        }
+
+        System.out.println(dp[N]);
+    }
+}
